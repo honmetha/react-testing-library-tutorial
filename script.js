57c9cbf2c59328158146fe7415fe4a9dc596049b
Hon <dennis.mejen@gmail.com>
2020-02-24 15:30:02 +0700

    Add answer to Ex 3, 4, 5

--- script.js
+++ script.js
@@ -21,5 +21,36 @@ delete obj.name;
 
 // Ex2
 function isEmpty(obj) {
-  Object.keys(obj).length == 0 ? false : true;
+  return (Object.keys(obj).length == 0 ? true : false); 
+}
+
+// Ex3
+"No Error"
+
+// Ex4: Hon
+function sum(obj) {
+  let sum = 0;
+  for (i = 0; i < Object.keys(obj).length; i++) {
+    sum = sum + obj[Object.keys(obj)[i]];
+  }
+  return sum;
+}
+
+// Ex4: Sonter
+function sum(obj) {
+  let total = 0;
+  for (let key in obj) {
+    total += obj[key];
+  }
+  return total;
+}
+
+// Ex5
+function multiplyNumeric(obj, times) {
+  for (let key in obj) {
+    if (typeof (obj[key]) == "number") {
+      obj[key] *= times
+    }
+  }
+  return obj;
 }
