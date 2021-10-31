640fe4beb10dbbceb42ac4c9c0cf0adeeb3ab73c
Hon <dennis.mejen@gmail.com>
2021-10-31 22:12:26 +0700

    [Added] 8 - Assertions (Part 2)

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -15,3 +15,39 @@ it("should render the correct amount of incomplete tasks", async () => {
   const paragraphElement = screen.getByText(/5 tasks left/i);
   expect(paragraphElement).toBeInTheDocument();
 });
+
+it("should render 'task' when the number of incomplete tasks is one", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+it("should render 'task' when the number of incomplete tasks is one", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeVisible();
+});
+
+it("should render 'task' when the number of incomplete tasks is one", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toContainHTML("p");
+});
+
+it("should render 'task' when the number of incomplete tasks is one", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("para");
+  expect(paragraphElement).toHaveTextContent("1 task left");
+});
+
+it("should render 'task' when the number of incomplete tasks is one", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("para");
+  expect(paragraphElement.textContent).not.toBeFalsy();
+});
+
+it("should render 'task' when the number of incomplete tasks is one", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("para");
+  expect(paragraphElement.textContent).toBe("1 task left");
+});
