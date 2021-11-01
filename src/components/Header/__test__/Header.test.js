a20221bcd8a2afaed9244bd5cbf42b1f8cfc7a40
Hon <dennis.mejen@gmail.com>
2021-11-01 23:43:58 +0700

    [Added] 9 - Describe Block

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-it("should render same text passed into title prop", async () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.getByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
+describe("Header", () => {
+  it("should render same text passed into title prop", async () => {
+    render(<Header title="My Header" />);
+    const headingElement = screen.getByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
 
 // it("should render same text passed into title prop", () => {
