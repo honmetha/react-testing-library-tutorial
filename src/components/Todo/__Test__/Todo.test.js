b4fc3994c3effa3cd703c47dc2be4b1fcb5da1a1
Hon <dennis.mejen@gmail.com>
2021-11-02 23:51:09 +0700

    [Added] 11 - Integration Tests (Part 1)

---
+++ src/components/Todo/__Test__/Todo.test.js
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Todo from "../Todo";
+
+const MockTodo = () => {
+  return (
+    <BrowserRouter>
+      <Todo />
+    </BrowserRouter>
+  );
+};
+
+describe("Todo", () => {
+  it("should render same text passed into title prop", async () => {
+    render(<MockTodo />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    const buttonElement = screen.getByRole("button", { name: /Add/i });
+    fireEvent.change(inputElement, {
+      target: { value: "Go Grocery Shopping" },
+    });
+    fireEvent.click(buttonElement);
+    const divElement = screen.getByText(/Go Grocery Shopping/i);
+    expect(divElement).toBeInTheDocument();
+  });
+});
