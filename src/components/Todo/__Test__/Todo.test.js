472860028eab8832ffaaf02fdb654e61dab18af2
Hon <dennis.mejen@gmail.com>
2021-11-03 23:50:44 +0700

    [Added] 11 - Integration Tests (Part 2)

--- src/components/Todo/__Test__/Todo.test.js
+++ src/components/Todo/__Test__/Todo.test.js
@@ -10,16 +10,44 @@ const MockTodo = () => {
   );
 };
 
+const addTask = (tasks) => {
+  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+  const buttonElement = screen.getByRole("button", { name: /Add/i });
+  tasks.forEach((task) => {
+    fireEvent.change(inputElement, {
+      target: { value: task },
+    });
+    fireEvent.click(buttonElement);
+  });
+};
+
 describe("Todo", () => {
   it("should render same text passed into title prop", async () => {
     render(<MockTodo />);
-    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-    const buttonElement = screen.getByRole("button", { name: /Add/i });
-    fireEvent.change(inputElement, {
-      target: { value: "Go Grocery Shopping" },
-    });
-    fireEvent.click(buttonElement);
+    addTask(["Go Grocery Shopping"]);
     const divElement = screen.getByText(/Go Grocery Shopping/i);
     expect(divElement).toBeInTheDocument();
+  });
+
+  it("should render multiple items", async () => {
+    render(<MockTodo />);
+    addTask(["Go Grocery Shopping", "Pet my Cat", "Wash my Hands"]);
+    const divElement = screen.getAllByTestId("task-container");
+    expect(divElement.length).toBe(3);
+  });
+
+  it("task should not have completed class when initially rendered", async () => {
+    render(<MockTodo />);
+    addTask(["Go Grocery Shopping"]);
+    const divElement = screen.getByText(/Go Grocery Shopping/i);
+    expect(divElement).not.toHaveClass("todo-item-active");
+  });
+
+  it("task should have completed class when clicked", async () => {
+    render(<MockTodo />);
+    addTask(["Go Grocery Shopping"]);
+    const divElement = screen.getByText(/Go Grocery Shopping/i);
+    fireEvent.click(divElement);
+    expect(divElement).toHaveClass("todo-item-active");
   });
 });
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -32,6 +32,7 @@ function TodoList({ todos, setTodos }) {
               className={`todo-item ${todo.completed && "todo-item-active"}`}
               onClick={() => updateTask(todo.id)}
               key={index}
+              data-testid="task-container"
             >
               {todo.task}
             </div>
