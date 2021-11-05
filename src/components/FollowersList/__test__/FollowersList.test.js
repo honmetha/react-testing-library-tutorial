4902395b6193480cc4d6d17f4dc08d7e2190053b
Hon <dennis.mejen@gmail.com>
2021-11-05 23:49:51 +0700

    [Added] 12 - Finding Async Elements with FindBy

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "../FollowersList";
+
+const MockFollowersList = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+
+describe("FollowersList", () => {
+  it("should render follower items", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+
+  it("should render multiple follower items", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findAllByTestId(/follower-item/i);
+    expect(followerDivElement.length).toBe(5);
+  });
+});
