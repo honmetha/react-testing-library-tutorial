847ff6f9dac1de97b139293b29eca7e0f8c66204
Hon <dennis.mejen@gmail.com>
2021-11-06 23:46:07 +0700

    [Added] 14 - Before &After Each

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -11,6 +11,34 @@ const MockFollowersList = () => {
 };
 
 describe("FollowersList", () => {
+  beforeEach(() => {
+    console.log("RUNNING BEFORE EACH TEST");
+  });
+
+  beforeAll(() => {
+    console.log("RAN ONCE BEFORE ALL TESTS");
+  });
+
+  afterEach(() => {
+    console.log("RUNNING AFTER TEST");
+  });
+
+  afterAll(() => {
+    console.log("RAN ONCE AFTER ALL TESTS");
+  });
+
+  it("should render follower items", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+
+  it("should render follower items", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+
   it("should render follower items", async () => {
     render(<MockFollowersList />);
     const followerDivElement = await screen.findByTestId("follower-item-0");
